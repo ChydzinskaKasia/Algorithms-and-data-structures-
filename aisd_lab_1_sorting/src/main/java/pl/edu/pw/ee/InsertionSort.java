7f1f0dbda923dfc7729d91461c977ad85a343dd7
Katarzyna Chydzińska <01167772@pw.edu.pl>
2021-10-11 13:56:53 +0200

    1st try

---
+++ aisd_lab_1_sorting/src/main/java/pl/edu/pw/ee/InsertionSort.java
@@ -0,0 +1,22 @@
+package pl.edu.pw.ee;
+
+import pl.edu.pw.ee.services.Sorting;
+
+public class InsertionSort implements Sorting {
+
+    @Override
+    public void sort(double[] nums) {
+        int numsLength = nums.length;
+
+        for (int i = 1; i < numsLength; i++) {
+            int j = i - 1;
+
+            while (i < numsLength) {
+
+            
+            }
+
+        }
+    }
+
+}
