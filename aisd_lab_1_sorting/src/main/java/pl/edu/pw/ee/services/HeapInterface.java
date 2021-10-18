573f09733658cf526c0d6047ef81f404cb0fad93
Katarzyna Chydzińska <01167772@pw.edu.pl>
2021-10-18 14:08:22 +0200

    LAB_1_2

---
+++ aisd_lab_1_sorting/src/main/java/pl/edu/pw/ee/services/HeapInterface.java
@@ -0,0 +1,19 @@
+package pl.edu.pw.ee;
+import pl.edu.pw.ee.services.HeapInterface;
+
+
+public class Heap<T extends Comparable<T>> implements HeapInterface<T> {
+
+    @Override
+    public void put(T item) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public T pop() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
