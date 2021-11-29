9184b1c4d339a5e6883b1e0c26059ffea4f90910
Katarzyna Chydzińska <01167772@pw.edu.pl>
2021-11-29 13:51:30 +0100

    LAB_5

---
+++ aisd_lab_7_huffman/src/main/java/pl/edu/pw/ee/Huffman.java
@@ -0,0 +1,28 @@
+package pl.edu.pw.ee;
+import java.util.Map;
+
+
+public class Huffman {
+
+    public Huffman(Map<Character,Integer>frequency){
+
+
+    }
+
+    
+    private Node root;
+    public int huffman(String pathToRootDir, boolean compress){
+    	// TODO
+    	return -1;
+    }
+
+
+    class Node implements Comparable<Node>{
+        private int frequency;
+        private char sign;
+        private Node leftChild;
+        private Node rightChild;
+
+
+    }
+}
