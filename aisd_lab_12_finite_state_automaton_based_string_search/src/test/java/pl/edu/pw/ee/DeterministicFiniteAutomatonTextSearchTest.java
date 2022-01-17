09193f1d3870b35e86799a0ac5917eb4ea62eede
Katarzyna Chydzińska <katarzyna.chydzinska.stud@pw.edu.pl>
2022-01-17 13:47:44 +0100

    LAB_8

---
+++ aisd_lab_12_finite_state_automaton_based_string_search/src/test/java/pl/edu/pw/ee/DeterministicFiniteAutomatonTextSearchTest.java
@@ -0,0 +1,17 @@
+package pl.edu.pw.ee;
+
+import org.junit.Test;
+
+import pl.edu.pw.ee.services.PatternSearch;
+
+import org.junit.Before;
+
+public class DeterministicFiniteAutomatonTextSearchTest {
+
+
+    @Before
+    public void setUp() {
+        //deterministicFiniteAutomatonTextSearch = new DeterministicFiniteAutomatonTextSearch();
+    }
+
+}
